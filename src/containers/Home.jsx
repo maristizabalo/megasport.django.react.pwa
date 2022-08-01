757c0f485289294f8ed285c8f3d15ba010e5b8a1
Maicol Jacobo Aristizabal Obando <mjaristizabal@dadep.gov.co>
2022-08-01 16:34:05 -0500

    first commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../hocs/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+        <div className='text-blue-600'>
+            Maik
+        </div>
+    </Layout>
+  )
+}
+
+export default Home
