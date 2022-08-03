a8b41118db10a820a3534d80df9f0f54f9071487
Maicol <maristizabalo95@gmail.com>
2022-08-03 00:36:44 -0500

    second commit

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <h1 className='text-center font-sans text-9xl text-clip text-lime-500'>Footer</h1>
   )
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,9 +4,7 @@ import Layout from '../hocs/Layout'
 const Home = () => {
   return (
     <Layout>
-        <div className='text-blue-600'>
-            Maik
-        </div>
+        <h1 className='text-center font-sans text-9xl'>Home</h1>
     </Layout>
   )
 }
