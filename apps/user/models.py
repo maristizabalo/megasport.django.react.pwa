fca19c887cb0741c34efb833f691e1d017f13116
Maicol Jacobo Aristizabal Obando <mjaristizabal@dadep.gov.co>
2022-08-12 17:10:16 -0500

    en oficina

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self,email,password=None, **extra_fields):
-        
+    def create_user(self,email,password=None, **extra_fields):     
         if not email:
             raise ValueError('Usuario debe tener un email')
         
@@ -33,7 +32,19 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    obejcts = UserAccountManager()
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.email
 
 
 
